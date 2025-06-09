c702bfb6f4ed722477b2b3e50c6d0843b5885565
Caelan Miller <98186985+caelanjmiller@users.noreply.github.com>
2025-06-09 14:58:57 -0500

    Revamped Lab 1 to substring match rubric

--- Lab-01-Practice-Programming/lab_one.py
+++ Lab-01-Practice-Programming/lab_one.py
@@ -1,20 +1,19 @@
-# Task 1: IO
-
+print("Task 1: Input/Output")
 # User prompt for demographics
-first_name: str = input("What is your first name?: ")
-last_name: str = input("What is your last name?: ")
+first_name: str = input("Enter your first name: ")
+last_name: str = input("Enter your last name: ")
 # Input is of str type, so type cast to integer
-age: int = int(input("What is your age?: "))
+age: int = int(input("Enter your age: "))
 
 # Print out demographic information
 print(f"Hello, {first_name} {last_name}! You are {age} years old.")
 
-# Task 2: Variables
+print("\nTask 2: Variables")
 
 # Account for real prices as floats
-item_one: float = float(input("Price of Item One: "))
-item_two: float = float(input("Price of Item Two: "))
-item_three: float = float(input("Price of Item Three: "))
+item_one: float = float(input("Enter the price of item 1: "))
+item_two: float = float(input("Enter the price of item 2: "))
+item_three: float = float(input("Enter the price of item 3: "))
 
 # Group all three numbers in list & calculate sum over it
 subtotal: float = sum([item_one, item_two, item_three])
@@ -22,23 +21,22 @@ tax: float = subtotal * 0.085
 total: float = subtotal + tax
 
 # Print all three values
-print(f"Subtotal: {subtotal:.2f}\nTax: {tax:.2f}\nTotal: {total:.2f}")
+print(f"Subtotal: ${subtotal:.2f}\nTax: ${tax:.2f}\nTotal: ${total:.2f}")
 
-# Task 3: String Basics
-favorite_color: str = input("What is your favorite color?: ")
-print(f"Length of your favorite color: {len(favorite_color)} characters")
+print("\nTask 3: String Basics")
+favorite_color: str = input("Enter your favorite color: ")
+print(f"The length is {len(favorite_color)}.")
 
-# Task 4: Number Basics
-num_1: float = float(input("Number 1: "))
-num_2: float = float(input("Number 2: "))
-results: float = sum([num_1, num_2])
-print(f"Sum of Number 1 & 2: {results}")
+print("\nTask 4: Number Basics")
+num_1: float = float(input("Enter first number: "))
+num_2: float = float(input("Enter second number: "))
+results: int = int(sum([num_1, num_2]))
+print(f"The sum is {results}.")
 
-# Task 5: Error Message
-print('Original statement:  print(Hello" ')
-print('Corrected statement: print("Hello")')
+print("\nTask 5: Error Message")
+print("Hello")
 
-# Task 6: Comments
+print("\nTask 6: Comments")
 
 # Creating a function that prints both an inline program and docstring; returns None since we're only printing
 def string_and_docstring() -> None:
