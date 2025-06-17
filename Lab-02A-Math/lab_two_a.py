d42fe024d41d3a2fa88bccf243461cdd3dfca666
Caelan Miller <98186985+caelanjmiller@users.noreply.github.com>
2025-06-16 22:02:25 -0500

    Completed first task of Lab 2A

---
+++ Lab-02A-Math/lab_two_a.py
@@ -0,0 +1,20 @@
+import math
+import random
+
+# Calculate the Square and Cube of a Random Number:
+random_generated_number: int = random.randint(1,100)
+print(f"Square of {random_generated_number} is: {math.sqrt(random_generated_number)}")
+print(f"Cube of {random_generated_number} is: {math.pow(random_generated_number, 3)}")
+
+
+# Calculate the Square Root of a Random Number:
+#     Generate another random integer between 1 and 100.
+#     Calculate and print the square root of this number using the math module.
+
+# Experiment with Randomness:
+#     Use the random module to generate a random float between 0.0 and 1.0.
+#     Multiply this float by a constant to explore how randomness can be scaled in different ranges.
+
+# Explore Math Constants:
+#     Print the values of pi and e from the math module, and describe what they are used for in mathematics.
+
