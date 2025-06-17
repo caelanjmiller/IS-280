0acdac64cf80f91b066c1c632de6425b0e5af3ef
Caelan Miller <98186985+caelanjmiller@users.noreply.github.com>
2025-06-16 22:27:30 -0500

    Completed Lab 2A

--- Lab-02A-Math/lab_two_a.py
+++ Lab-02A-Math/lab_two_a.py
@@ -3,18 +3,21 @@ import random
 
 # Calculate the Square and Cube of a Random Number:
 random_generated_number: int = random.randint(1,100)
-print(f"Square of {random_generated_number} is: {math.sqrt(random_generated_number)}")
-print(f"Cube of {random_generated_number} is: {math.pow(random_generated_number, 3)}")
-
+print(f"The square of {random_generated_number} is: {math.pow(random_generated_number, 2)}")
+print(f"The cube of {random_generated_number} is: {math.pow(random_generated_number, 3)}")
 
 # Calculate the Square Root of a Random Number:
-#     Generate another random integer between 1 and 100.
-#     Calculate and print the square root of this number using the math module.
+another_random_generated_number: int = random.randint(1,100)
+print(f"Square Root of {random_generated_number} is: {math.sqrt(another_random_generated_number)}")
 
 # Experiment with Randomness:
-#     Use the random module to generate a random float between 0.0 and 1.0.
-#     Multiply this float by a constant to explore how randomness can be scaled in different ranges.
+random_float: float = random.random()
+print(f"Random float between 0.0 and 1.0: {random_float}")
+float_meets_constant: float = random_float * 6
+print(f"Scaled random float between 0.0 and 100: {float_meets_constant}")
 
 # Explore Math Constants:
-#     Print the values of pi and e from the math module, and describe what they are used for in mathematics.
-
+print(math.pi)
+print(f"Pi is a mathematical constant that is defined by the ratio of a circle's circumference to its diameter")
+print(math.e)
+print(f"e, or Euler's number, is the base of the natural log, which is quite important for investigating both exponential growth and decay")
