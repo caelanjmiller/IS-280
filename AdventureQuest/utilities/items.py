27bc05870c701148f818475625c6baa2441b967a
Caelan Miller <98186985+caelanjmiller@users.noreply.github.com>
2025-07-20 23:29:01 -0500

    Added logic for updating status effects for character

--- AdventureQuest/utilities/items.py
+++ AdventureQuest/utilities/items.py
@@ -26,6 +26,13 @@ def drop_item(character: dict, items_at_location: dict, item: str):
         char.remove_from_inventory(character, item)
         items_at_location[(x,y)].append(item)
 
+def use_item(character: dict, item: str):
+    """
+    Applies the item's effect and removes it from the inventory
+    """
+    effects: dict = item_effects[item]
+    char.update_character(character, effects)
+
 def manage_items(command: str, character: dict, items_at_location: dict):
     """
     Routes item commands (pick up, drop & use) to appropriate handlers
@@ -37,3 +44,37 @@ def manage_items(command: str, character: dict, items_at_location: dict):
     
     elif command.startswith('d '):
         pick_up_item(character, items_at_location, item)
+
+item_effects: dict = {
+    "Health Potion": {
+        "health": 20
+    },
+    "Magic Scroll": {
+        "magic": 10
+    },
+    "Food": {
+
+    },
+    "Water": {
+
+    },
+    "Shield": {
+        "strength": 5
+    },
+    "Old Sword": {
+        "strength": 10
+    },
+    "Enchanted Amulet": {
+        "health": 5,
+        "magic": 5
+    },
+    "Ancient Scroll": {
+        "level": 1
+    },
+    "Gold Coin": {
+
+    },
+    "Enchanted Sword": {
+        "strength": 15
+    },
+}
