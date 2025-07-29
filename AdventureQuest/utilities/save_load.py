4f20573281075e1031f4699b5705e7983add667c
Caelan Miller <98186985+caelanjmiller@users.noreply.github.com>
2025-07-29 17:52:46 -0500

    Revamped save_game() functionality

--- AdventureQuest/utilities/save_load.py
+++ AdventureQuest/utilities/save_load.py
@@ -39,21 +39,37 @@ def save_game(character, location_items):
     """
     Save state of Adventure Quest into a text file
     """
+    try:
+        with open(filename, "r") as save_file:
+            lines = save_file.readlines()
+    except FileNotFoundError:
+        lines = []
+
+    i = 0
+    while i < len(lines) and lines[i].strip() != "---":
+        i += 1
+    if i < len(lines):
+        i += 1
+
+    save_lines = []
+    save_lines.append("Character\n")
+    save_lines.append(f"{character['name']},{character['health']},{character['strength']},"
+                f"{character['level']},{character['gold']},{character['magic']},"
+                f"{character['location'][0]},{character['location'][1]}\n")
+    save_lines.append("Inventory," + ",".join(character['inventory']) + "\n")
+    save_lines.append("ItemsAtLocation\n")
+    for loc, items in location_items.items():
+        line = f"{loc[0]},{loc[1]}"
+        if items:
+            line += "," + ",".join(items)
+        save_lines.append(line + "\n")
+    for quest, info in character['quests'].items():
+        save_lines.append(f"{quest},{info['status']}\n")
+    save_lines.append("---\n")
+
+    lines = lines[:i] + save_lines + lines[i:]
     with open(filename, "a") as save_file:
-        save_file.write("Character\n")
-        save_file.write(f"{character['name']},{character['health']},{character['strength']},"
-                   f"{character['level']},{character['gold']},{character['magic']},"
-                   f"{character['location'][0]},{character['location'][1]}\n")
-        save_file.write("Inventory," + ",".join(character['inventory']) + "\n")
-        save_file.write("ItemsAtLocation\n")
-        for loc, items in location_items.items():
-            line = f"{loc[0]},{loc[1]}"
-            if items:
-                line += "," + ",".join(items)
-            save_file.write(line + "\n")
-        for quest, info in character['quests'].items():
-            print(f"{quest},{info['status']}")
-        save_file.write("---\n")
+        save_file.writelines(lines)
 
 def load_game(character_name):
     """
