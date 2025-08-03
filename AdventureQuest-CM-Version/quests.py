cba877b1f82451e7886a3ad35a0830daac11bb0d
Caelan Miller <98186985+caelanjmiller@users.noreply.github.com>
2025-08-03 15:00:55 -0500

    Renamed quests.py to correct title

---
+++ AdventureQuest-CM-Version/quests.py
@@ -0,0 +1,11 @@
+"""
+Module aims to:
+1. Define quests for Adventure Quest
+2. Handle acceptance & completion of quests
+3. Checking current quest(s) progress
+4. Check for quest/object interaction based upon location
+"""
+
+class Quest():
+    def __init__(self) -> None:
+        pass
